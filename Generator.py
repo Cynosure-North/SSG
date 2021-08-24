d75bf7de76c2ace27fc6cd97cc397f51474a5cbb
Cynosure-North <52474045+Cynosure-North@users.noreply.github.com>
2021-08-24 20:36:50 +1200

    MVP
    TODO:
    Header files
    Expand Markdown or Custom?
    Dither PNGs
    Command Line Interface

--- Generator.py
+++ Generator.py
@@ -1,52 +1,47 @@
 import os
 import tkinter.filedialog
 import shutil
-#import dither
+import markdown2
 
-#import dither
-#import MD_to_HTML
-
-#Flags
+# Flags
 dither_png = True
-
-
-source_dir = tkinter.filedialog.askdirectory(title="Choose source folder")
-target_dir = tkinter.filedialog.askdirectory(title="Choose target folder")
-target_dir = os.path.join(target_dir, os.path.basename(source_dir))
-
-print(source_dir)
-print(target_dir)
 
 
 def walk(source_dir, target_dir):
     if os.path.exists(target_dir):
-        pass
-        #Todo Remove
+        if input(target_dir + " already exists, do you wish to continue. (y/n").lower() == 'n':
+            exit()
     os.mkdir(target_dir)
-    for (root, dirs, files) in os.walk(source_dir):
-        #Ignore hidden files
-        files = [f for f in files if not f[0] == '.']
-        dirs[:] = [d for d in dirs if not d[0] == '.']
 
-        #Copy the folder Structure
-        current_folder = os.path.relpath(root, source_dir)
-        if root != source_dir:
-            os.mkdir(os.path.join(target_dir, current_folder))
-        #Copy the files can convert if needed
-        for file in files:
-            if file.endswith(".md"):
-                MD_to_HTML.convert(file)
-            else:
-                shutil.copyfile(os.path.join(source_dir, current_folder, file), os.path.join(target_dir, current_folder, file))
-                
-    ''' #TODO: Dither
-            elif file.endswith(".png") and dither_png:
-                pass
-                #dither.dither(file
-    '''
+    with open("template.html", "r") as template:
+        for (root, dirs, files) in os.walk(source_dir):
+            # Ignore hidden files
+            files = [f for f in files if not f[0] == '.']
+            dirs[:] = [d for d in dirs if not d[0] == '.']
 
-def copy(file):
-    pass
+            # Copy the folder Structure
+            current_folder = os.path.relpath(root, source_dir)
+            if root != source_dir:
+                os.mkdir(os.path.join(target_dir, current_folder))
+            # Copy the files
+            for file in files:
+                # If they're markdown convert it
+                if file.endswith(".md"):
+                    content = markdown2.markdown(open(os.path.join(source_dir, root, file), "r").read())
+                    with open(os.path.join(target_dir, root, os.path.splitext(file)[0] + ".html"), "w") as output:
+                        for line in template:
+                            output.write(line.replace("[Content]", content))
+
+                else:
+                    shutil.copyfile(os.path.join(source_dir, current_folder, file),
+                                    os.path.join(target_dir, current_folder, file))
 
 
-walk(source_dir, target_dir)
+source = tkinter.filedialog.askdirectory(title="Choose source folder")
+target = tkinter.filedialog.askdirectory(title="Choose target folder")
+target = os.path.join(target, os.path.basename(source))
+
+print(source)
+print(target)
+
+walk(source, target)
